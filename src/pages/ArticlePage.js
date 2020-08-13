df8ada02b72cc60a9909c738da351c8cda893ecb
kaustubh <kaustubh285@gmail.com>
2020-08-13 16:05:53 +0530

    Upvotes fetch working properly

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import articleContent from "./article-content";
 import ArticleList from "../components/ArticleList";
 import PageNotFound from "./PageNotFound";
@@ -7,6 +7,25 @@ export default function ArticlePage({ match }) {
   const name = match.params.name;
 
   const article = articleContent.find((article) => article.name === name);
+
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`/api/article/${name}`);
+      const body = await result.json();
+      setArticleInfo(body);
+    };
+    fetchData();
+  }, [name]);
+
+  // useEffect(() => {
+  //   console.log("updates!");
+  //   setArticleInfo({ upvotes: Math.ceil(Math.random() * 10) });
+  //* }, []);
+  //? We pass an empty array to useEffect to tell it, if anyone of the variable present in the array changes, then exec the function again.
+  //? But the issue is when a different article is loaded, it doesnt execute the useEffect again.
+  // * That is why we passed [name] in the array. So whenever the name AKA the Url changes, it executes again, i.e for every article
 
   const relatedArticles = articleContent.filter(
     (article) => article.name !== name
@@ -25,7 +44,17 @@ export default function ArticlePage({ match }) {
           <u>{article.title}</u>
         </h1>
       </div>
+
       <div className='w-75 mx-auto mt-5 '>
+        <div>
+          <p className='text-right'>
+            This post has {articleInfo.upvotes} upvotes{" "}
+            <i style={{ fontSize: 25 }} className='fa'>
+              {" "}
+              &#xf087;
+            </i>
+          </p>
+        </div>
         {article.content.map((paragraph, key) => (
           <p key={key} style={{ fontWeight: "400", textAlign: "justify" }}>
             {paragraph}
